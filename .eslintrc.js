fbc51cd0263901f9790574cb0eb7987f2be946e0
Austin Howard <ahoward@austinhoward.tech>
2023-01-16 18:12:40 -0500

    chore(all): migrate to new npm org package scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["@rocket-science-core/eslint-config-custom"],
+  extends: ["@rocket-science/eslint-config-custom"],
 };
